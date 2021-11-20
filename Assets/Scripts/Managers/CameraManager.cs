4975ff86628841f21acdc98cdc0fc3a1a04fa01f
윤재 노 <yjroh0928@naver.com>
2021-11-20 12:08:20 +0900

    camera moving

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 playerTransform;
+    public float moveSpeed;
+    private float CameraX;
+    private float CameraY;
+
     private void Awake()
     {
         Init();
     }
     public void Init()
     {
-
     }
     void Start()
     {
-        
     }
 
     void Update()
     {
-        
+        if (player.gameObject != null)
+        {
+
+            CameraX = Mathf.Clamp(this.transform.position.x, player.transform.position.x - 3, player.transform.position.x + 3);
+            CameraY = Mathf.Clamp(this.transform.position.y, player.transform.position.y - 3, player.transform.position.y + 3);
+            this.transform.position = new Vector3(CameraX, CameraY , this.transform.position.z);
+        }
     }
 
     internal void Clear()
