5688be34ace6011349c3f0c3dd75a4c48ac190d6
J.Han <dubious123@hanyang.ac.kr>
2021-11-20 18:48:28 +0900

    bug fixed : ball sticks to wall

--- Assets/Scripts/UI/Btn_Map.cs
+++ Assets/Scripts/UI/Btn_Map.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Btn_Map : MonoBehaviour
 {
     [SerializeField] Button _Btn;
     [SerializeField] GameObject _Lock;
     [SerializeField] GameObject _UnLock;
+    [SerializeField] TextMeshProUGUI _Text;
+    void Start()
+    {
+        _Btn.onClick.AddListener(()=>SceneManager.LoadScene($"{SceneManager.GetActiveScene().name.Split(' ')[1]}-{transform.GetSiblingIndex() + 1}"));
+    }
     public void Lock()
     {
         _Lock.SetActive(true);
@@ -18,6 +25,7 @@ public class Btn_Map : MonoBehaviour
     {
         _Lock.SetActive(false);
         _UnLock.SetActive(true);
+        _Text.text = (transform.GetSiblingIndex() + 1).ToString();
         _Btn.enabled = true;
     }
 }
